eaa81ee671f27a904265214d51173bd5014c2c09
mdmehedihasan-dev <mdmehedihasandev0@gmail.com>
2025-06-24 21:08:31 +0600

    design

--- src/Pages/Settings/AboutUS/AboutUs.jsx
+++ src/Pages/Settings/AboutUS/AboutUs.jsx
@@ -17,29 +17,28 @@ const AboutUs = () => {
 
   return (
     <div className="container min-h-screen mt-16 ">
-      
-                  <Link
+      <Link
         to={"/"}
         className="flex items-center mt-16 mb-6 text-white gap-x-3"
       >
         <FaArrowLeftLong size={20} />
-        <h1 className="text-2xl font-semibold ">  About Us</h1>
+        <h1 className="text-2xl font-semibold "> About Us</h1>
       </Link>
-    <di className="mt-5" >
+      <di className="mt-5">
         <JoditEditor
-        ref={editor}
-        value={content}
-        config={config}
-        tabIndex={1}
-        onBlur={(newContent) => setContent(newContent)}
-        onChange={() => {}}
-      />
-   <div className="text-center">
-       <button className="bg-[#5F9E19] p-2 text-white  mt-2 rounded-lg">
-        Save Change
-      </button>
-   </div>
-    </di>
+          ref={editor}
+          value={content}
+          config={config}
+          tabIndex={1}
+          onBlur={(newContent) => setContent(newContent)}
+          onChange={() => {}}
+        />
+        <div className="text-center">
+          <button className="bg-[#5F9E19] p-2 text-white  mt-2 rounded-lg">
+            Save Change
+          </button>
+        </div>
+      </di>
     </div>
   );
 };
